65ab410cb032e74037fb8cde97e04ff6477f2a46
sachin-sankar <mail.sachinsankar@gmail.com>
2024-09-05 20:21:27 +0530

    test: Added tests for getXlsxUrl

--- tests/test_secapi.py
+++ tests/test_secapi.py
@@ -2,7 +2,7 @@ import pytest
 
 from edgarpy.exceptions import InvalidCIK
 from edgarpy.models import Submission
-from edgarpy.secapi import getSubmissionsByCik
+from edgarpy.secapi import getSubmissionsByCik, getXlsxUrl
 
 
 def test_getSubmissionsByCik_fails_on_empty_cik():
@@ -13,3 +13,13 @@ def test_getSubmissionsByCik_fails_on_empty_cik():
 def test_getSubmissionsByCik():
     out = getSubmissionsByCik("0001018724")
     assert isinstance(out[0], Submission)
+
+
+def test_getXlsxUrl_fails_on_invalid_cik():
+    with pytest.raises(FileNotFoundError) as _:
+        getXlsxUrl("", "")
+
+
+def test_getXlsxUrl():
+    out = getXlsxUrl("0001018724", "0001018724-24-000130")
+    assert isinstance(out, str)
